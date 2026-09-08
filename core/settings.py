eb8986ec79a00849f6c58f2d56c6c327f8cb5b7b
Amanwith884 <hatamianamirhossein8@gmail.com>
2026-09-08 10:29:16 -0700

    chore: finalize Docker infrastructure and fix MAILERS config
    - Create Dockerfile and .dockerignore for Django container
    - Set up docker-compose.yml with PostgreSQL and healthchecks
    - Fix database connection race condition (depends_on service_healthy)
    - Update email settings to use modern Django MAILERS structure
    - Add LOGIN and LOGOUT redirect URLs for local dev testing

--- core/settings.py
+++ core/settings.py
@@ -197,8 +197,26 @@ MAILERS = {
     },
 }
 
-LOGIN_REDIRECT_URL = "tasks:team_list"
+LOGIN_REDIRECT_URL = "/api/"
 LOGOUT_REDIRECT_URL = "/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+if DEBUG:
+    MAILERS = {
+        "default": {
+            "BACKEND": "django.core.mail.backends.console.EmailBackend",
+        }
+    }
+else:
+    MAILERS = {
+        "default": {
+            "BACKEND": "django.core.mail.backends.smtp.EmailBackend",
+            "HOST": os.environ.get('EMAIL_HOST', 'smtp.gmail.com'),
+            "PORT": int(os.environ.get('EMAIL_PORT', 587)),
+            "USE_TLS": os.environ.get('EMAIL_USE_TLS', 'True') == 'True',
+            "USER": os.environ.get('EMAIL_HOST_USER', ''),
+            "PASSWORD": os.environ.get('EMAIL_HOST_PASSWORD', ''),
+        }
+    }
